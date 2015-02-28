fc10edba56e4381a96e24ceac3704ba2a7c40f6f
Oisota <dman6505@gmail.com>
2015-02-28 00:41:08 -0500

    removed bin folder

--- breakout.py
+++ breakout.py
@@ -8,6 +8,6 @@ This module imports the game package and runs the game.
 """
 
 if __name__ == '__main__':
-    from breakout.game import Game
+    from ..breakout.game import Game
     game = Game()
     game.start()
