21240532ea0f4c11577114475143eaf5ee25d404
Oisota <dman6505@gmail.com>
2015-02-28 21:51:19 -0500

    removed superfluous imports

--- breakout/__init__.py
+++ breakout/__init__.py
@@ -4,12 +4,3 @@ Breakout Package.
 This package contains all the modules needed to 
 run the game.
 """
-
-#import breakout.game, breakout.ball, breakout.brick, breakout.paddle, breakout.player, breakout.menu, breakout.resource
-from breakout.game import Game
-from breakout.ball import Ball
-from breakout.brick import Brick
-from breakout.paddle import Paddle
-from breakout.player import Player
-from breakout.menu import Menu
-import resource
