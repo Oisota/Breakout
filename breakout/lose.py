870ceabf8cb20b10a249b59c48395695a41a3630
Oisota <dman6505@gmail.com>
2015-05-01 14:45:05 -0400

    lose/start/win now subclass menu scene

--- breakout/lose.py
+++ breakout/lose.py
@@ -15,51 +15,5 @@ class Lose(MenuScene):
         """Initialize the scene"""
         MenuScene.__init__(self, RES, 
                 'lose.png','retry.png','retry_pressed.png','quit.png','quit_pressed.png', 
-                lambda: self.goto(GamePlay(RES), lambda: self.terminate())
-        self.mouse_pos = (0,0)
-        self.pressed = ''
-        self.next_scene = self
-        self.background, self.bg_rect = resource.load_image('brickwall.png')
+                lambda: self.goto(GamePlay(RES)), lambda: self.terminate())
 
-        self.menu = Menu(RES) #construct menu
-        self.menu.addTitle(RES[0]/2, 100, 'lose.png') 
-        self.menu.addButton(RES[0]/2, 200, 'retry.png', 'retry_pressed.png', lambda: self.goto(Gameplay(RES)))
-        self.menu.addButton(RES[0]/2, 300, 'quit.png', 'quit_pressed.png', lambda: self.terminate())
-
-        pygame.event.set_allowed([QUIT, MOUSEMOTION, MOUSEBUTTONUP, MOUSEBUTTONDOWN])
-        pygame.mouse.set_visible(True)
-
-
-    def render(self, screen):
-        """Render the Scene"""
-        screen.blit(self.background, (0,0))
-        self.menu.draw(screen)
-
-
-    def update(self):
-        """Update the Scene"""
-        self.menu.update(self.mouse_pos, self.pressed)
-        #pygame.display.update(self.menu.rects)
-        pygame.display.update()
-
-
-    def handle_events(self):
-        """Handle Events"""
-        for event in pygame.event.get():
-            if event.type == QUIT:
-                self.terminate()
-            elif event.type == MOUSEBUTTONDOWN:
-                self.mouse_pos = event.pos
-                pressed = 'mouse ' + str(event.button)
-            elif event.type == MOUSEMOTION:
-                self.mouse_pos = event.pos
-
-
-    def goto(self, scene):
-        """change the scene"""
-        self.next_scene = scene
-
-
-    def terminate(self):
-        """End the scene"""
-        self.goto(None)
