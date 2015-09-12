7ddffd270146de023c86a4f957b2d0d8944402e2
Derek Morey <dman6505@gmail.com>
2015-09-11 21:33:09 -0400

    Added breakout-editor.py script
    The breakout-editor.py script was added. This script will launch
    the level editor.

---
+++ breakout-editor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Level Editor Module
+
+This module imports the editor package and starts the level editor.
+"""
+
+if __name__ == '__main__':
+    import editor.editor
+    editor.editor.run()
