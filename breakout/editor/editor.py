d48c35802d410de1b9e5c7107febf5eb9a7a8a1a
Derek Morey <dman6505@gmail.com>
2015-10-11 19:47:58 -0400

    General Update
    The editor module was updated with more functionality and updates.

--- breakout/editor/editor.py
+++ breakout/editor/editor.py
@@ -1,29 +1,31 @@
 """
 Game Module
 
-This module defines a run function. The run function
-initializes pygame, sets some game variables and runs 
-the game.
+This module defines the Editor class. This module runs
+the game's level editor.
 """
 
+import sys, os
 import tkinter as tk
 from tkinter.filedialog import askopenfilename, asksaveasfilename
-import sys
+
+from breakout.utils.resource import load_level, save_level
+
 
 class Editor(tk.Frame):
     """Main level editor frame."""
     def __init__(self, parent=None):
+        """Initialize the editor."""
         tk.Frame.__init__(self, parent)
         self.parent = parent
         self.level_filename = ''
+        self.brick_img = tk.PhotoImage(file='breakout/resources/images/brick_red.gif')
         self.level_name = tk.StringVar()
         self.ball_speed = tk.StringVar()
         self.next_level = tk.StringVar()
-        self.parent.geometry('800x600')
-        self.parent.title('Breakout Level Editor')
         self.grid()
         self.create_widgets()
-        self.pack_widgets()
+        self.grid_widgets()
 
 
     def create_widgets(self):
@@ -38,18 +40,41 @@ class Editor(tk.Frame):
         self.file_menu.add_command(label='Exit', command=self.quit)
         self.menu.add_cascade(label='File', menu=self.file_menu)
 
-        self.lvl_nm_lbl = tk.Label(self, text='Level Name: ')
-        self.ball_spd_lbl = tk.Label(self, text='Ball Speed: ')
-        self.nxt_lvl_lbl = tk.Label(self, text='Next Level: ')
+        #create input boxes
+        self.input_frm = tk.Frame(self)
+        self.lvl_nm_lbl = tk.Label(self.input_frm, text='Level Name: ')
+        self.ball_spd_lbl = tk.Label(self.input_frm, text='Ball Speed: ')
+        self.nxt_lvl_lbl = tk.Label(self.input_frm, text='Next Level: ')
+
+        self.lvl_nm_tb = tk.Entry(self.input_frm, textvariable=self.level_name)
+        self.ball_spd_tb = tk.Entry(self.input_frm, textvariable=self.ball_speed)
+        self.nxt_lvl_tb = tk.Entry(self.input_frm, textvariable=self.next_level)
+
+        #create button grid
+        #TODO put button frame in separate class
+        self.button_frm = tk.Frame(self)
+        self.buttons = []
+        for i in range(10):
+            btn_row = []
+            for j in range(10):
+                button = tk.Button(self.button_frm, image=self.brick_img, width=72, height=27)
+                btn_row.append(button)
+
+            self.buttons.append(btn_row)
 
 
-        self.lvl_nm_tb = tk.Entry(self, textvariable=self.level_name)
-        self.ball_spd_tb = tk.Entry(self, textvariable=self.ball_speed)
-        self.nxt_lvl_tb = tk.Entry(self, textvariable=self.next_level)
 
+    def grid_widgets(self):
+        """position widgets in the frame."""
+        #place buttons
+        self.button_frm.grid(row=0, column=0)
+        self.button_frm['pady'] = 35
+        self.button_frm['padx'] = 10
+        for i, row in enumerate(self.buttons):
+            for j, btn in enumerate(row):
+                btn.grid(row=i, column=j)
 
-    def pack_widgets(self):
-        """Pack widgets into the frame."""
+        self.input_frm.grid(row=1, column=0)
         self.lvl_nm_lbl.grid(row=0, column=0, sticky='E')
         self.ball_spd_lbl.grid(row=1, column=0, sticky='E')
         self.nxt_lvl_lbl.grid(row=2, column=0, sticky='E')
@@ -62,11 +87,14 @@ class Editor(tk.Frame):
     def save_level(self):
         """Save the level file."""
         self.level_filename = asksaveasfilename()
+        resource.save_level(self.level_filename)
         
         
     def open_level(self):
         """Open the level file."""
         self.level_filename = askopenfilename()
+        if self.level_filename != '':
+            self.level = load_level(os.path.basename(self.level_filename))
 
 
     def quit(self):
@@ -75,9 +103,32 @@ class Editor(tk.Frame):
 
 
 
+class ButtonGrid(tk.Frame):
+    """Frame that contains grid of brick buttons"""
+    def __init__(self, parent=None):
+        """Initialze the button grid"""
+        tk.Frame.__init__(self, parent)
+        self.parent = parent
+        self.create_widgets()
+        self.grid_widgets()
+
+
+    def create_widgets(self):
+        """create grid of buttons"""
+        pass
+
+
+    def grid_widgets(self):
+        """Position buttons."""
+        pass
+        
+
+
 
 def run():
     """Run the Editor.""" 
     root = tk.Tk()
+    root.geometry('800x600')
+    root.title('Breakout Level Editor')
     editor = Editor(root)
     editor.mainloop()
